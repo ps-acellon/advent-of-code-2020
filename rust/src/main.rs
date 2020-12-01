cb21d21d8d920d48eb1e40d28892aa910eae444e
ps-acellon <adam.cellon@crisphealth.org>
2020-12-01 17:19:38 -0500

    Pull out day 1 changes into separate file

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,48 +1,8 @@
+pub mod day01;
+
 fn main() {
     day01::part1();
     day01::part2();
-}
-
-mod day01 {
-    use crate::aoc::read_lines;
-    const SUM: i32 = 2020;
-
-    pub fn part1() {
-        let expenses: Vec<i32> = read_lines("src/input-day01.txt")
-            .into_iter()
-            .map(|line| line.parse::<i32>().unwrap())
-            .collect();
-        for expense in expenses.iter() {
-            let summand = SUM - expense;
-            if expenses.contains(&summand) {
-                println!("Day 1, part 1: {}", expense * summand);
-                return;
-            }
-        }
-    }
-
-    pub fn part2() {
-        let mut expenses: Vec<i32> = read_lines("src/input-day01.txt")
-            .into_iter()
-            .map(|line| line.parse::<i32>().unwrap())
-            .collect();
-        expenses.sort();
-        for (first_idx, first_entry) in expenses.iter().enumerate() {
-            for second_idx in (first_idx + 1)..expenses.len() {
-                for third_idx in (first_idx + 2)..expenses.len() {
-                    let second_entry = expenses[second_idx];
-                    let third_entry = expenses[expenses.len() - third_idx];
-                    if first_entry + second_entry + third_entry == SUM {
-                        println!(
-                            "Day 1, part 2: {}",
-                            first_entry * second_entry * third_entry
-                        );
-                        return;
-                    }
-                }
-            }
-        }
-    }
 }
 
 mod aoc {
