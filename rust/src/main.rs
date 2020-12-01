43e4b6c48cd6a017fe8e20314d6a0a8dc6ca0825
ps-acellon <adam.cellon@crisphealth.org>
2020-12-01 16:52:15 -0500

    Make it faster

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,14 +3,30 @@ fn main() {
     day01::part2();
 }
 
-pub mod day01 {
-    use std::fs::File;
-    use std::io::{self, BufRead};
-    use std::path::Path;
+mod day01 {
+    use crate::aoc::read_lines;
+    const SUM: i32 = 2020;
+
+    pub fn part1() {
+        let expenses: Vec<i32> = read_lines("src/input-day01.txt")
+            .into_iter()
+            .map(|line| line.parse::<i32>().unwrap())
+            .collect();
+        for expense in expenses.iter() {
+            let summand = SUM - expense;
+            if expenses.contains(&summand) {
+                println!("Day 1, part 1 - {}", expense * summand);
+                return
+            }
+        }
+    }
 
     pub fn part2() {
-        let lines = read_lines("src/input-day01.txt");
-        let expenses = get_sorted_expenses(lines);
+        let mut expenses: Vec<i32> = read_lines("src/input-day01.txt")
+            .into_iter()
+            .map(|line| line.parse::<i32>().unwrap())
+            .collect();
+        expenses.sort();
         for (idx, expense) in expenses.iter().enumerate() {
             for front_offset in (idx + 1)..expenses.len() {
                 for offset in (idx + 1)..expenses.len() {
@@ -24,36 +40,21 @@ pub mod day01 {
             }
         }
     }
+}
 
-    pub fn part1() {
-        let lines = read_lines("src/input-day01.txt");
-        let expenses = get_sorted_expenses(lines);
-        for (idx, expense) in expenses.iter().enumerate() {
-            for offset in (idx + 1)..expenses.len() {
-                let summand = expenses[expenses.len() - offset];
-                if expense + summand == 2020 {
-                    println!("Day 1, part 2 - {}", expense * summand);
-                    return;
-                }
-            }
-        }
-    }
+mod aoc {
+    use std::fs::File;
+    use std::io::{self, BufRead};
+    use std::path::Path;
 
-    fn get_sorted_expenses(lines: io::Lines<io::BufReader<File>>) -> Vec<i32> {
-        let mut expenses: Vec<i32> = lines
-            .into_iter()
-            .map(|line| line.unwrap())
-            .map(|line| line.parse::<i32>().unwrap())
-            .collect();
-        expenses.sort();
-        expenses
-    }
-
-    fn read_lines<P>(filename: P) -> io::Lines<io::BufReader<File>>
+    pub fn read_lines<P>(filename: P) -> Vec<String>
     where
         P: AsRef<Path>,
     {
         let file = File::open(filename).expect("Unable to open file.");
-        io::BufReader::new(file).lines()
+        io::BufReader::new(file)
+            .lines()
+            .map(|line| line.unwrap())
+            .collect()
     }
 }
